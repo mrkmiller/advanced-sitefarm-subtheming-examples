1a79c131d6c1290c01bfd51a1df94c463aad7fb5
Mark Miller <mrkmiller@ucdavis.edu>
2019-02-27 18:16:20 -0800

    render people via jsonapi

--- vue_blocks/my_vue_demo/script.js
+++ vue_blocks/my_vue_demo/script.js
@@ -1,20 +1,39 @@
 // This will only work on a single block in a page.
 // https://www.drupal.org/docs/8/modules/decoupled-blocks-vuejs/simple-per-block-vue-instances
 
-const myDemo = new Vue({
+var myDemo = new Vue({
   el: '.my-vue-demo',
 
-  // template: '<div>{{ message }}</div>',
-
   data: {
-    message: 'Hello SiteFarmers!'
+    people: [],
   },
 
   // It can be helpful to get the Block Instance ID that Drupal uses. This ID
   // can be used to fetch data that Drupal has set about the block.
   beforeMount: function () {
-    const id = this.$el.id;
+    var id = this.$el.id;
     console.log('Vue instance ID: ' + id);
+  },
+
+  created: function () {
+    this.getPeople();
+  },
+
+  methods: {
+    getPeople: function () {
+      var url = 'http://sitefactory-demo.local/jsonapi/node/sf_person';
+
+      var params = {
+        sort: 'field_sf_last_name',
+        include: 'field_sf_primary_image'
+      };
+
+      var self = this;
+
+      jQuery.get(url, params, function (data) {
+        self.people = window.jsonapi.parse(data).data;
+      });
+    }
   }
 
 });
